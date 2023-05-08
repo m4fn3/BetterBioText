3357eff14a8a82c973a56e9ff39284ca8d3aaa66
mafusuke <mafusukee@gmail.com>
2023-05-08 23:34:09 +0900

    First

---
+++ src/index.tsx
@@ -0,0 +1,55 @@
+import {Plugin, registerPlugin} from 'enmity/managers/plugins'
+import {React, Linking, Toasts} from 'enmity/metro/common'
+import {create} from 'enmity/patcher'
+// @ts-ignore
+import manifest, {name as plugin_name} from '../manifest.json'
+import Settings from "./components/Settings"
+import {getByName} from "enmity/metro"
+import {getByProps} from "enmity/modules";
+import {getIDByName} from "enmity/api/assets";
+
+const Patcher = create('BetterBioText')
+
+const BioTexts = getByName("BioText", {default: false, all: true})
+const Clipboard = getByProps('setString')
+const CopyIcon = getIDByName('ic_message_copy')
+
+const BetterBioText: Plugin = {
+    ...manifest,
+    onStart() {
+        BioTexts.forEach((BioText) => {
+            Patcher.after(BioText, "default", (self, args, res) => {
+                res.props.onLongPress = () => {
+                    Clipboard.setString(args[0].bio)
+                    Toasts.open({
+                        content: `Copied bio text to clipboard`,
+                        source: CopyIcon
+                    })
+                }
+                for (const obj of res.props.children) {
+                    if (obj?.props?.accessibilityRole === "link") {
+                        let link = obj.props.children[0]
+                        obj.props.onPress = () => {
+                            Linking.openURL(link)
+                        }
+                        obj.props.onLongPress = () => {
+                            Clipboard.setString(link)
+                            Toasts.open({
+                                content: `Copied URL to clipboard`,
+                                source: CopyIcon
+                            })
+                        }
+                    }
+                }
+            })
+        })
+    },
+    onStop() {
+        Patcher.unpatchAll()
+    },
+    getSettingsPanel({settings}) {
+        return <Settings settings={settings}/>
+    }
+}
+
+registerPlugin(BetterBioText)
